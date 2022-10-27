952e55f82bdfaef37504fec2c44dd6d3ae2d67b3
chierighini <chierighini.andre@gmail.com>
2022-10-26 22:32:10 -0300

    Add sec groups and instances

--- ec2.tf
+++ ec2.tf
@@ -5,9 +5,38 @@ resource "aws_instance" "jenkins-instance" {
   subnet_id                   = aws_subnet.main_public.id
   associate_public_ip_address = true
   key_name                    = aws_key_pair.ssh-key-jenkins.key_name
-  user_data                   = file("install_jenkins.sh")
+  user_data                   = file("user_data/install_jenkins.sh")
 
   tags = {
     Name = "Jenkins-Instance"
   }
+}
+
+resource "aws_instance" "k8s_node" {
+  ami                         = "ami-0f72d3c899140e51a"
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.main_public.id
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
+
+
+  tags = {
+    Name = "K8s-Node"
+  }
+
+}
+
+resource "aws_instance" "k8s_master" {
+  ami                         = "ami-0f72d3c899140e51a"
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.main_public.id
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = ["${aws_security_group.k8s_master_sg.id}"]
+
+  tags = {
+    Name = "K8s-Master"
+  }
+
 }
--- sg.tf
+++ sg.tf
@@ -1,8 +1,9 @@
 resource "aws_security_group" "sg_allow_ssh_jenkins" {
   name        = "allow_ssh_jenkins"
   description = "Allow SSH and Jenkins inbound traffic"
-  vpc_id      = aws_vpc.example_vpc.id
+  vpc_id      = aws_vpc.main_vpc.id
 
+  # SSH
   ingress {
     from_port   = 22
     to_port     = 22
@@ -10,6 +11,7 @@ resource "aws_security_group" "sg_allow_ssh_jenkins" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Jenkins web traffic
   ingress {
     from_port   = 8080
     to_port     = 8080
@@ -17,11 +19,101 @@ resource "aws_security_group" "sg_allow_ssh_jenkins" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 1
-    to_port     = -1
-    protocol    = "icmp"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# https://kubernetes.io/docs/reference/ports-and-protocols/
+
+resource "aws_security_group" "k8s_node_sg" {
+  name   = "k8s_node"
+  vpc_id = aws_vpc.main_vpc.id
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_protocol = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Kubelet API
+  ingress {
+    from_port                = 10250
+    to_protocol              = 10250
+    protocol                 = "tcp"
+    source_security_group_id = aws_security_group.k8s_master_sg.id
+  }
+
+  # NodePort Servicest
+  ingress {
+    from_port                = 30000
+    to_protocol              = 32767
+    protocol                 = "tcp"
+    source_security_group_id = aws_security_group.k8s_master_sg.id
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "k8s_master_sg" {
+  name   = "k8s_master"
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_protocol = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Kubernetes API Server
+  ingress {
+    from_port                = 6443
+    to_protocol              = 6443
+    protocol                 = "tcp"
+    source_security_group_id = aws_security_group.k8s_node_sg.id
+  }
+
+  # etcd server client API
+  ingress {
+    from_port                = 2379
+    to_protocol              = 2380
+    protocol                 = "tcp"
+    source_security_group_id = aws_security_group.k8s_node_sg.id
+  }
+
+  # Kubelet API
+  ingress {
+    from_port                = 10250
+    to_protocol              = 10250
+    protocol                 = "tcp"
+    source_security_group_id = aws_security_group.k8s_node_sg.id
+  }
+
+  # kube-scheduler
+  ingress {
+    from_port                = 10259
+    to_protocol              = 10259
+    protocol                 = "tcp"
+    source_security_group_id = aws_security_group.k8s_node_sg.id
+  }
+
+  # kube-controller-manager
+  ingress {
+    from_port                = 10257
+    to_protocol              = 10257
+    protocol                 = "tcp"
+    source_security_group_id = aws_security_group.k8s_node_sg.id
   }
 
   egress {
