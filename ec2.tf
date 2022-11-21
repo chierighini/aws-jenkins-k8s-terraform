b21a3bce05ae475ac6c59cbc01125dc945be43d1
chierighini <chierighini.andre@gmail.com>
2022-11-20 22:22:14 -0300

    add public ip to master

--- ec2.tf
+++ ec2.tf
@@ -13,11 +13,12 @@ resource "aws_instance" "k8s_node" {
 }
 
 resource "aws_instance" "k8s_master" {
-  ami                    = "ami-0f72d3c899140e51a"
-  instance_type          = "t3.small"
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = ["${aws_security_group.k8s_master_sg.id}"]
-  key_name               = var.ssh_key_name
+  ami                         = "ami-0f72d3c899140e51a"
+  instance_type               = "t3.small"
+  subnet_id                   = var.subnet_id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = ["${aws_security_group.k8s_master_sg.id}"]
+  key_name                    = var.ssh_key_name
 
   tags = {
     Name = "K8s-Master"
