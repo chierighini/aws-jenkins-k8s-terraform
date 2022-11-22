7e7582e4ab3e31b5eca911e463aa0f9bf4277ace
chierighini <chierighini.andre@gmail.com>
2022-11-22 19:26:02 -0300

    typo

--- eks.tf
+++ eks.tf
@@ -6,7 +6,7 @@ module "eks" {
   cluster_version = "1.22"
 
   vpc_id     = var.vpc_id
-  subnet_ids = [var.subnet_ids]
+  subnet_ids = var.subnet_ids
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
